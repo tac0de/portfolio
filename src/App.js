13339b6dff7ac81ce1ebd3bb4326d9e99cedd628
tac0de <wonyoungchoiseoul@gmail.com>
2020-11-27 15:55:39 +0900

    folder structure changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./layouts/Header";
-import Footer from "./layouts/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./styles/App.css";
 
 import { sections } from "./utils/variables";
